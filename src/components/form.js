61b5eca0e94915049867e7362a35287e11512d12
Julian Marmier <julianmarmier@gmail.com>
2020-04-21 01:00:36 -0400

    Try form again

--- src/components/form.js
+++ src/components/form.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import Recaptcha from 'react-google-recaptcha'
+//import Recaptcha from 'react-google-recaptcha'
 
-const RECAPTCHA_KEY = process.env.GATSBY_APP_SITE_RECAPTCHA_KEY
-if (typeof RECAPTCHA_KEY === 'undefined') {
-  throw new Error(`
-  Env var GATSBY_APP_SITE_RECAPTCHA_KEY is undefined!
-  You probably forget to set it in your Netlify build environment variables.
-  Make sure to get a Recaptcha key at https://www.netlify.com/docs/form-handling/#custom-recaptcha-2-with-your-own-settings
-  Note this demo is specifically for Recaptcha v2
-  `)
-}
+//const RECAPTCHA_KEY = process.env.GATSBY_APP_SITE_RECAPTCHA_KEY
+// if (typeof RECAPTCHA_KEY === 'undefined') {
+//   throw new Error(`
+//   Env var GATSBY_APP_SITE_RECAPTCHA_KEY is undefined!
+//   You probably forget to set it in your Netlify build environment variables.
+//   Make sure to get a Recaptcha key at https://www.netlify.com/docs/form-handling/#custom-recaptcha-2-with-your-own-settings
+//   Note this demo is specifically for Recaptcha v2
+//   `)
+// }
 
 export default function Form() {
     return (
@@ -18,13 +18,13 @@ export default function Form() {
           className="contactform"
           method="post"
           data-netlify="true"
-          data-netlify-recaptcha="true"
+          netlify-honeypot="bot-field"
         >
           <input type="hidden" name="form-name" value="contact-form"/>
           <input placeholder="Name" type="text" name="name" />
           <input placeholder="Email Address" type="email" name="email" />
           <textarea placeholder="Message" name="message" />
-          <div className="submit"><Recaptcha sitekey={RECAPTCHA_KEY} /><button type="submit">Send</button></div>
+          <div className="submit"><button type="submit">Send</button></div>
         </form>
   )
 }
