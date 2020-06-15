6d8152dd89042e7409cbd35c1da8f27bc28d29b5
julianmarmier <julianmarmier@gmail.com>
2020-06-15 18:39:28 -0400

    Updates to the chapter structure

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,10 +3,62 @@ const { fmImagesToRelative } = require('gatsby-remark-relative-images');
 const remark = require("remark");
 const remarkHTML = require("remark-html");
 
-exports.onCreateNode = ({ node }) => {
+const { createFilePath } = require(`gatsby-source-filesystem`);
+const path = require(`path`)
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+
   fmImagesToRelative(node);
-  if (node.frontmatter.about) {
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+
+  /* if (typeof(node.frontmatter.about) !== 'undefined') {
     const markdown = node.frontmatter.about;
     node.frontmatter.about = remark().use(remarkHTML).processSync(markdown).toString();
-  }
-};
+  } */
+}
+
+exports.createPages = ({graphql, actions}) => {
+  const { createPage } = actions;
+  const chapterTemplate = path.resolve(`src/templates/chapter.js`);
+  return graphql(`
+    query loadPagesQuery {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+            }
+          }
+        }
+      }
+    }
+  `).then((result) => {
+    if (result.errors) {
+      throw result.errors;
+    }
+
+    result.data.allMarkdownRemark.edges.forEach((edge) => {
+        createPage({
+          path: `${edge.node.fields.slug}`,
+          component: chapterTemplate,
+          context: {
+            title: edge.node.title
+          }
+        })
+    });
+
+
+  })
+}
--- src/templates/chapter.js
+++ src/templates/chapter.js
@@ -12,8 +12,8 @@ import "../css/index.css"
 
 const ChapterPage = ({props}) => {
   const data = useStaticQuery(graphql`
-    query {
-      chapter: markdownRemark(frontmatter: {title: {eq: "Boston"}}) {
+    query chapterQuery($title: String) {
+      chapter: markdownRemark(frontmatter: {title: {eq: $title}}) {
         frontmatter {
           title
           about
@@ -56,12 +56,12 @@ const ChapterPage = ({props}) => {
     }
   `)
   const chapter = data.chapter.frontmatter;
-  const BackgroundImageStack = [`linear-gradient(to bottom, #${chapter.colorOne}88, rgba(255, 0, 0, 0) 50%)`, chapter.mainImage.childImageSharp.fluid]
+  const BackgroundImageStack = [`linear-gradient(to bottom, ${chapter.colorTwo}88, rgba(255, 0, 0, 0) 50%)`, chapter.mainImage.childImageSharp.fluid]
   return (
     <Layout>
       <Img className="hero-image" fluid={BackgroundImageStack} alt="Masks for Hunger">
         <h1 className="hero-main" id="top">{chapter.header}</h1>
-        <Link className="arrow-down" to="/#about">
+        <Link className="arrow-down" to="#about">
           <DownArrow />
         </Link>
       </Img>
@@ -69,7 +69,7 @@ const ChapterPage = ({props}) => {
         <span className="anchor" id="about"></span>
         <div className="title about" id="about-container"><h4>About</h4></div>
         <div className="left profile about">
-          <DivImg className="profile-pic" style={{backgroundAttachment: 'fixed'}} fluid={data.second.childImageSharp.fluid}/>
+          <DivImg className="profile-pic" style={{backgroundAttachment: 'fixed'}} fluid={chapter.profileImage.childImageSharp.fluid}/>
           <div className="profile-card"><h3>{chapter.leader}}</h3><p>{chapter.subtitle}</p></div>
         </div>
         <div className="right about">
@@ -86,9 +86,9 @@ const ChapterPage = ({props}) => {
         <p>Masks for Hunger is an initiative to raise awareness about food for all during this crisis. You're welcome to donate without getting a mask. Just <a href="https://secure.projectbread.org/site/Donation2?idb=1934012782&df_id=6233&FR_ID=1400&mfc_pref=T&PROXY_ID=2304152&PROXY_TYPE=20&6233.donation=form1&pw_id=3761&s_AffiliateSecCatId=2341&NONCE_TOKEN=0D63D32F6732BC089ED848A192544239">click here</a> to donate!</p>
         <p>However, if you do wish to get a mask, just follow these simple steps:</p>
         <ol>
-          <li><a href="https://secure.projectbread.org/site/Donation2?idb=1934012782&df_id=6233&FR_ID=1400&mfc_pref=T&PROXY_ID=2304152&PROXY_TYPE=20&6233.donation=form1&pw_id=3761&s_AffiliateSecCatId=2341&NONCE_TOKEN=0D63D32F6732BC089ED848A192544239">Make a donation on the Walk for Hunger website</a>. A pledge of at least $25 is recommended to get a mask.</li>
-          <li>Browse the selection of masks <Link to="/#catalog">on the catalog</Link>.</li>
-          <li>Tell me which masks you would like by submitting the <Link to="/#contact">contact form</Link> below, or send me an email with your name, address (if you would like me to deliver to you), and the masks you would like. Submit two separate forms if you would like more than 8.</li>
+          <li><a href={chapter.donateURL}>Make a donation on the Walk for Hunger website</a>. A pledge of at least $25 is recommended to get a mask.</li>
+          <li>Browse the selection of masks <Link to="#catalog">on the catalog</Link>.</li>
+          <li>Tell me which masks you would like by submitting the <Link to="#contact">contact form</Link> below, or send me an email with your name, address (if you would like me to deliver to you), and the masks you would like. Submit two separate forms if you would like more than 8.</li>
           <li>You should receieve a response by mail from me shortly.</li>
         </ol>
         <p>Again, you aren't required to get a mask to contribute! All remaining masks will be given to local hospitals or non-profits on the frontline.</p>
@@ -108,7 +108,7 @@ const ChapterPage = ({props}) => {
         })}</div>
         <div className="title contact" id="contact-container"><h4>Contact</h4></div>
       <span className="anchor" id="contact"></span>
-      <div className="left contact section"><p>If you wish to get a mask or have any other questions, please fill out this form or email me at <a href="mailto:lylagvideos@gmail.com">lylagvideos@gmail.com</a></p></div>
+      <div className="left contact section"><p>If you wish to get a mask or have any other questions, please fill out this form or email me at <a href={`mailto:${chapter.contactEmail}`}>{chapter.contactEmail}</a></p></div>
     <div className="right contact"><Form maskList={chapter.masks.map((mask) => (mask.quantity === 0 ? null : mask.title))} /></div>
       </div>
       <div className="donate-section"><h1>Support the movement!</h1><a href={chapter.donateURL} className="donate-btn">Donate now on the Walk for Hunger website →</a></div>
